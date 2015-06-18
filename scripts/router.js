140a2c23d13c760996b5330acac7d733125b73ae
Matt Chastain <matthewdchastain@gmail.com>
2015-06-18 15:52:51 -0400

    Actually finished static mockup

---
+++ scripts/router.js
@@ -0,0 +1,20 @@
+var Router = Backbone.Router.extend({
+
+	routes: {
+		'': 'index',
+	},
+
+	initialize: function() {
+
+	},
+
+	index: function() {
+
+	},
+
+
+
+});
+
+var router = new Router();
+export default router;
