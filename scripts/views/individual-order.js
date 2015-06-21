ed3ca3627366d85156c7f4fd0ebfef90ef5bb82f
Matt Chastain <matthewdchastain@gmail.com>
2015-06-21 15:25:35 -0400

    Added Admin page to complete orders; working on modal contact form

---
+++ scripts/views/individual-order.js
@@ -0,0 +1,28 @@
+
+
+export default Backbone.View.extend({
+
+	template: JST['individual-order'],
+
+	tagName: 'div',
+	className: 'order-container-individual',
+
+	events: {
+		'click .order-completed': 'completeOrder'
+	},
+
+	initialize: function() {
+		this.render();
+	},
+
+	render: function() {
+		this.$el.html(this.template(this.model));
+	},
+
+	completeOrder: function() {
+		if (confirm("Are you sure this order is completed?")) {
+			this.collection.remove(this.model);
+		}
+	}
+
+});
